bf0d90351bb8b99232b2f84eafd3ca8b9ba8e58b
albert <albert.cymbaluk@gmail.com>
2017-09-23 17:50:51 +0200

    new not working

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '852205733678-rn60uul707nqm661urbuun9hvf6spvbr.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'q8RyF0uYcxt--VWhydu9Vl8l',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
